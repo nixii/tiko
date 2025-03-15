45cddbe52081389e4e985aa0fe9e86086bca9f41
Nixii <161391079+nixii@users.noreply.github.com>
2025-03-15 10:35:51 -0700

    Fixed memory leak

--- src/main.c
+++ src/main.c
@@ -7,12 +7,26 @@
 // Main function
 int main(void) {
 
+  // Get the lexer result of this static string
   LexerResult res = tokenize("#150 #20");
+
+  // If there isn't an error and there are actually tokens
   if (res.tokens && !Deque_isEmpty(res.tokens)) {
+
+    // While the deque still has tokens
     while (!Deque_isEmpty(res.tokens)) {
+
+      // Get the token
       Token *t = Deque_popFront(res.tokens);
+
+      // Print the token
       Token_repr(t);
+
+      // FREE THE TOKEN
+      free(t); // IMPORTANT: Free the token!
     }
   }
+
+  // Return 0
   return 0;
 }
